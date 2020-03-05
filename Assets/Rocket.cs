649a249b07cc4cf6431b127c79abe7c6abe5fe50
Maria Kovaleva <mariakovaleva@outlook.com>
2020-03-05 12:52:15 -0800

    added basic controls and audio
    Space to thrust
    A to rotate left
    D to rotate right
    Rocket takeoff sound

---
+++ Assets/Rocket.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Increases the speed of rocket's rotation (the higher, the faster)
+    [SerializeField] float rcsThrust = 100f;
+
+    [SerializeField] float mainThrust = 100f;
+
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float thrustThisFrame = mainThrust * Time.deltaTime;
+
+            //can thrust while rotating
+            rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false; //resume physics control of rotation
+
+    }
+}
